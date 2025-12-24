fe34657d84fb0cbf73fb4783ed5f611b5161d445
Aline Vernier <aline.vernier@ensta.fr>
2025-12-24 19:54:30 +0100

    Building interface dynamically is fun
    > Need to play with threads
    > Get data asynchronously for each graph from events!
    > Still quite a bit of work...

--- LaserData.py
+++ LaserData.py
@@ -1,10 +1,20 @@
 import tango
+from PyQt6.QtWidgets import QApplication
+from PyQt6 import QtCore
+import sys
+import qdarkstyle
 from Devices import DeviceMaker
-class Laser_Data:
+from Build_Interface import Monitoring_Interface
 
-    def __init__(self):
+
+class Laser_Data(Monitoring_Interface):
+
+    def __init__(self, polling_period: float, buffer_size: int=1000):
+        super().__init__(buffer_size)
         self.setup()
         self.devices = {}
+        self.polling_period = polling_period
+        self._buffer_size = 1000
 
     def setup(self):
         try:
@@ -21,20 +31,33 @@ class Laser_Data:
         beam_analyzer = dict({('name', 'Beam Analyzer 1'),
                              ('address', "FE_CAMERA_1/ImgBeamAnalyzer/1"),
                              ('type', 'beam analyzer')})
+        energy_meter = dict({('name', 'Energy Meter 1'),
+                             ('address', "FE_EM_1/Energymeter/1"),
+                             ('type', 'energy meter')})
+        dummy_device = dict({('name', 'Dummy device 1'),
+                             ('address', ""),
+                             ('type', 'dummy device')})
 
-        self.device_list = [spectrometer, beam_analyzer]
+        self.device_list = [dummy_device]
 
     def create_devices(self):
         for dev in self.device_list:
-            self.devices[dev['name']] = DeviceMaker.create(dev)
+            try:
+                self.devices[dev['name']] = DeviceMaker.create(dev)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
-    laser_data = Laser_Data()
+
+    appli = QApplication(sys.argv)
+    appli.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt6'))
+    laser_data = Laser_Data(polling_period=1)
     laser_data.load_config()
     laser_data.create_devices()
-    print(f'Laser data devices: {laser_data.devices}')
-    #for dev_key in laser_data.devices:
-    #    print(laser_data.devices[dev_key])
+    for dev_key in laser_data.devices:
+        print(laser_data.devices[dev_key])
+    laser_data.show()
+    appli.exec_()
 
 
