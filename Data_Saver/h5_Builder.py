46232e6efdd6cf6d5ced4110f919a7200fed149a
Aline Vernier <aline.vernier@polytechnique.edu>
2026-01-06 21:21:39 +0100

    Building data saver

--- Data_Saver/h5_Builder.py
+++ Data_Saver/h5_Builder.py
@@ -1,56 +1,73 @@
 import h5py
 import numpy as np
+import pathlib 
 
 class h5Builder:
-    def __init__(self):
-        pass
+    def __init__(self, file: pathlib.Path):
+        self.file = file
 
     def create_file(self, device_list: list):
         # Define column names and types
         self.columns = []
         for device in device_list:
-            device_id = device['name']
-            self.columns = self.columns.append((device_id, 'f4'))
-        dt = np.dtype(self.columns)
+            if device.graph_type == 'rolling_1d':
+                device_id = device['name']
+                self.columns = self.columns.append((device_id, 'f4'))
+                dt = np.dtype(self.columns) # data type
+
+        with h5py.File(self.file, 'a') as f:
+            f.create_dataset(
+                'laser data',
+                shape=(0,),              # Start with 0 rows
+                maxshape=(None,),        # Unlimited rows
+                dtype=dt,
+                chunks=True              # Enable chunking for resizing
+            )
+
+    def append_data(self, data: np.ndarray):
+
+        with h5py.File(self.file, 'a') as f:
+            dataset = f['laser data']
+
+
+            # Resize and append
+            current_size = dataset.shape[0]
+            dataset.resize(current_size + len(data), axis=0)
+            dataset[current_size:] = data
 
 
 
 
-# Create or open an HDF5 file
-with h5py.File('realtime_data.h5', 'a') as f:
-    # Define your column names and data types
-    dt = np.dtype([
-        ('timestamp', 'f8'),      # float64
-        ('temperature', 'f4'),     # float32
-        ('pressure', 'f4'),        # float32
-        ('sensor_id', 'i4')        # int32
-    ])
 
-    # Create a resizable dataset if it doesn't exist
-    if 'measurements' not in f:
-        dataset = f.create_dataset(
-            'measurements',
-            shape=(0,),              # Start with 0 rows
-            maxshape=(None,),        # Unlimited rows
-            dtype=dt,
-            chunks=True              # Enable chunking for resizing
-        )
-    else:
-        dataset = f['measurements']
+if __name__ == "__main__":
 
-    # Append new data
-    new_data = np.array([
-        (1234567890.0, 25.5, 1013.25, 1),
-        (1234567891.0, 25.6, 1013.20, 1),
-    ], dtype=dt)
+    h5_file = h5Builder('realtime_data.h5')
+    config_dict = {
+    "device_1": {
+            "name": "Dummy device 1",
+            "address": "",
+            "type": "dummy device"
+        },
+        "device_2": {
+            "name": "Dummy device 2",
+            "address": "",
+            "type": "dummy device 1D"
+        },
+        "device_3": {
+            "name": "Dummy device 3",
+            "address": "",
+            "type": "dummy device 2D"
+        },
+        "device_4": {
+            "name": "Dummy device 4",
+            "address": "",
+            "type": "dummy device 2D"
+        }
+    }
+    device_list = [config_dict[key] for key in config_dict]
+    h5_file.create_file(device_list)
 
-    # Resize and append
-    current_size = dataset.shape[0]
-    dataset.resize(current_size + len(new_data), axis=0)
-    dataset[current_size:] = new_data
-
-# Read the data back
-with h5py.File('realtime_data.h5', 'r') as f:
-    data = f['measurements'][:]
-    print(f"Temperature column: {data['temperature']}")
-    print(f"All data: {data}")
+    # Read the data back
+    with h5py.File('realtime_data.h5', 'r') as f:
+        data = f['laser data'][:]
+        print(f"All data: {data}")
