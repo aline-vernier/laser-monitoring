54683d2a1df429204b92e702d464089d38ea27e2
Aline Vernier <aline.vernier@polytechnique.edu>
2026-01-07 11:33:38 +0100

    Continuing laser monitoring... Saving data
    > Still need to integrate Semion's laplace server
    > Visualization to be polished

--- Data_Saver/h5_Builder.py
+++ Data_Saver/h5_Builder.py
@@ -2,33 +2,40 @@ import h5py
 import numpy as np
 import pathlib 
 
-class h5Builder:
+
+class H5Builder:
     def __init__(self, file: pathlib.Path):
         self.file = file
 
-    def create_file(self, device_list: list):
+    def create_file(self, devices: list):
         # Define column names and types
         self.columns = []
-        for device in device_list:
+        for device in devices.values():
             if device.graph_type == 'rolling_1d':
-                device_id = device['name']
-                self.columns = self.columns.append((device_id, 'f4'))
-                dt = np.dtype(self.columns) # data type
+                print(f'Graph type: {device.graph_type}')
+                device_id = device.name
+                self.columns.append((device_id, 'f4'))
+            self.dt = np.dtype(self.columns) # data type
 
         with h5py.File(self.file, 'a') as f:
-            f.create_dataset(
-                'laser data',
-                shape=(0,),              # Start with 0 rows
-                maxshape=(None,),        # Unlimited rows
-                dtype=dt,
-                chunks=True              # Enable chunking for resizing
-            )
+            # Create a resizable dataset if it doesn't exist
+            if 'laser data' not in f:
+                f.create_dataset(
+                    'laser data',
+                    shape=(0,),              # Start with 0 rows
+                    maxshape=(None,),        # Unlimited rows
+                    dtype=self.dt,
+                    chunks=True              # Enable chunking for resizing
+                )
+
+            print(f'dt: {self.dt}')
+            print(f'columns: {self.columns}')
 
     def append_data(self, data: np.ndarray):
 
         with h5py.File(self.file, 'a') as f:
-            dataset = f['laser data']
 
+            dataset = f['laser data']
 
             # Resize and append
             current_size = dataset.shape[0]
@@ -36,38 +43,38 @@ class h5Builder:
             dataset[current_size:] = data
 
 
-
-
-
 if __name__ == "__main__":
 
-    h5_file = h5Builder('realtime_data.h5')
-    config_dict = {
-    "device_1": {
-            "name": "Dummy device 1",
-            "address": "",
-            "type": "dummy device"
-        },
-        "device_2": {
-            "name": "Dummy device 2",
-            "address": "",
-            "type": "dummy device 1D"
-        },
-        "device_3": {
-            "name": "Dummy device 3",
-            "address": "",
-            "type": "dummy device 2D"
-        },
-        "device_4": {
-            "name": "Dummy device 4",
-            "address": "",
-            "type": "dummy device 2D"
-        }
-    }
-    device_list = [config_dict[key] for key in config_dict]
-    h5_file.create_file(device_list)
+    from Device_Classes.Devices import DeviceMaker
+    from Config.Config_RW import readConfig
+    from random import uniform
+
+    devices = {}
+    numPoints = 100
+    data = np.array([range(0, numPoints)]).T
+
+    h5_file = H5Builder('./Data_Saver/realtime_data.h5')
+    config_file_path = "./Config/dummy_config.json"
+    
+    # Read configuration back from file
+    loaded_config = readConfig(config_file_path)
+    device_list = [loaded_config[key] for key in loaded_config]
+
+    for dev in device_list:
+        # Dictionary of device objects
+        device_id = dev['name']
+        device = DeviceMaker.create(dev)
+        devices[device_id] = device
+        dev_data=[]
+        for iter in range(0, numPoints):
+            dev_data.append(uniform(8, 12))
+        data = np.c_[data, dev_data]
+    print(data)
+
+
+    h5_file.create_file(devices)
 
     # Read the data back
-    with h5py.File('realtime_data.h5', 'r') as f:
+    with h5py.File('./Data_Saver/realtime_data.h5', 'r') as f:
         data = f['laser data'][:]
         print(f"All data: {data}")
